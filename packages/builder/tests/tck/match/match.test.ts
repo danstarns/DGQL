28eab60d4c699ff07f0c798a465b413f698396a1
Daniel Starns <danielstarns@hotmail.com>
2021-03-20 21:26:33 +0000

    test: add many match

--- packages/builder/tests/tck/match/match.test.ts
+++ packages/builder/tests/tck/match/match.test.ts
@@ -29,4 +29,70 @@ describe("match", () => {
 
         expect(params).toEqual({});
     });
+
+    test("should match and return many nodes (with 1 match)", () => {
+        const builder = new Builder();
+
+        const [dgql, params] = builder
+            .match({
+                node1: node({ label: "Node1" }),
+                node2: node({ label: "Node2" }),
+            })
+            .return(["node1", "node2"])
+            .build();
+
+        expect(print(parse(dgql))).toEqual(
+            print(
+                parse(`
+                    {
+                        MATCH {
+                            node1 @node(label: Node1)
+                            node2 @node(label: Node2)
+                        }
+                        RETURN {
+                            node1
+                            node2
+                        }
+                    }
+                `)
+            )
+        );
+
+        expect(params).toEqual({});
+    });
+
+    test("should match and return many nodes (with many match)", () => {
+        const builder = new Builder();
+
+        const [dgql, params] = builder
+            .match({
+                node1: node({ label: "Node1" }),
+            })
+            .match({
+                node2: node({ label: "Node2" }),
+            })
+            .return(["node1", "node2"])
+            .build();
+
+        expect(print(parse(dgql))).toEqual(
+            print(
+                parse(`
+                    {
+                        MATCH {
+                            node1 @node(label: Node1)
+                        }
+                        MATCH {
+                            node2 @node(label: Node2)
+                        }
+                        RETURN {
+                            node1
+                            node2
+                        }
+                    }
+                `)
+            )
+        );
+
+        expect(params).toEqual({});
+    });
 });
