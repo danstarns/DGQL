23c3824e5c52d43a7f9ec4ebd0c7305639a13847
Daniel Starns <danielstarns@hotmail.com>
2021-04-04 22:08:55 +0100

    feat: add better auto complete for directives

--- packages/playground/src/DGQLEditor.tsx
+++ packages/playground/src/DGQLEditor.tsx
@@ -1,92 +1,164 @@
 import React from "react";
 import Editor, { Monaco } from "@monaco-editor/react";
 
-function createDependencyProposals(range, monaco: Monaco) {
-    // returning a static list of proposals, not even looking at the prefix (filtering is done by the Monaco editor),
-    // here you could do a server side lookup
-    return [
-        {
-            label: "Node",
-            kind: monaco.languages.CompletionItemKind.Field,
-            documentation: "A Node",
-            insertText: "node @node",
-            range: range,
-        },
-    ];
-}
-
-function createRootSuggestions(range, monaco: Monaco) {
-    // returning a static list of proposals, not even looking at the prefix (filtering is done by the Monaco editor),
-    // here you could do a server side lookup
-    return [
-        {
-            label: "MATCH",
-            kind: monaco.languages.CompletionItemKind.Field,
-            documentation: "MATCH",
-            insertText: "MATCH {}",
-            range: range,
-        },
-    ];
-}
-
 function DGQLEditor({
-    onMount,
+  onMount,
 }: {
-    onMount: (editor: typeof Editor, monaco: Monaco) => void;
+  onMount: (editor: typeof Editor, monaco: Monaco) => void;
 }) {
-    function beforeMount(monaco: Monaco) {
-        monaco.languages.registerCompletionItemProvider("graphql", {
-            provideCompletionItems: (model, position) => {
-                // find out if we are completing a property in the 'dependencies' object.
-                const textUntilPosition = model.getValueInRange({
-                    startLineNumber: 1,
-                    startColumn: 1,
-                    endLineNumber: position.lineNumber,
-                    endColumn: position.column,
-                });
-
-                const word = model.getWordUntilPosition(position);
-                const range = {
-                    startLineNumber: position.lineNumber,
-                    endLineNumber: position.lineNumber,
-                    startColumn: word.startColumn,
-                    endColumn: word.endColumn,
-                };
-
-                console.log(textUntilPosition);
-                const isInMatch = textUntilPosition.match(
-                    /MATCH[\s\S]*{[\s\S]*$/
-                );
-                const atTop = textUntilPosition.match(
-                    /[\s\S]*{[\s\S]*(MATCH[\s\S]*{[\s\S]*})?[\s\S]*[^MATCH[\s\S]*[{][\s\S]*]+$/
-                );
-                if (atTop && !isInMatch) {
-                    return {
-                        suggestions: createRootSuggestions(range, monaco),
-                    };
-                }
-
-                if (isInMatch && !atTop) {
-                    return {
-                        suggestions: createDependencyProposals(range, monaco),
-                    };
-                }
-
-                return { suggestions: [] };
-            },
+  function beforeMount(monaco: Monaco) {
+    monaco.languages.registerCompletionItemProvider("graphql", {
+      provideCompletionItems: (model, position) => {
+        // find out if we are completing a property in the 'dependencies' object.
+        const textUntilPosition = model.getValueInRange({
+          startLineNumber: 1,
+          startColumn: 1,
+          endLineNumber: position.lineNumber,
+          endColumn: position.column,
         });
-    }
 
-    return (
-        <Editor
-            height="100vh"
-            theme="vs-dark"
-            defaultLanguage="graphql"
-            beforeMount={beforeMount}
-            onMount={onMount}
-            options={{ fontSize: "18", tabSize: 2 }}
-        />
-    );
+        const word = model.getWordUntilPosition(position);
+        const range = {
+          startLineNumber: position.lineNumber,
+          endLineNumber: position.lineNumber,
+          startColumn: word.startColumn,
+          endColumn: word.endColumn,
+        };
+
+        const isSelectingDirective = textUntilPosition.match(
+          /[\s\S]*@(?![\s\S])/
+        );
+
+        if (isSelectingDirective) {
+          return {
+            suggestions: [
+              {
+                label: "@node",
+                insertText: "node(label: Node)",
+                range: range,
+              },
+              {
+                label: "@edge",
+                insertText: "edge(type: HAS_NODE, direction: OUT)",
+                range: range,
+              },
+              {
+                label: "@paginate",
+                insertText: "paginate(skip: 1, limit: 10)",
+                range: range,
+              },
+              {
+                label: "@where",
+                insertText: 'where(id: "some-id")',
+                range: range,
+              },
+            ],
+          };
+        }
+
+        const isInNode = textUntilPosition.match(/[\s\S]*@node\([\s\S]*/);
+
+        if (isInNode) {
+          return {
+            suggestions: [
+              {
+                label: "label",
+                insertText: "label: Node",
+                range: range,
+              },
+            ],
+          };
+        }
+
+        const isInEdge = textUntilPosition.match(/[\s\S]*@edge\([\s\S]*/);
+
+        if (isInEdge) {
+          const hasType = textUntilPosition.match(
+            /[\s\S]*@edge\([\s\S]*type:[\s\S]*/
+          );
+          const hasDirection = textUntilPosition.match(
+            /[\s\S]*@edge\([\s\S]*direction:[\s\S]*/
+          );
+
+          if (hasType && hasDirection) {
+            return {
+              suggestions: [],
+            };
+          }
+
+          if (hasType && !hasDirection) {
+            return {
+              suggestions: [
+                {
+                  label: "direction",
+                  insertText: ", direction: OUT",
+                  range: range,
+                },
+              ],
+            };
+          }
+
+          if (hasDirection) {
+            return {
+              suggestions: [
+                {
+                  label: "type",
+                  insertText: ", type: HAS_NODE",
+                  range: range,
+                },
+              ],
+            };
+          }
+
+          return {
+            suggestions: [
+              {
+                label: "type",
+                insertText: "type: HAS_NODE",
+                range: range,
+              },
+              {
+                label: "direction",
+                insertText: "direction: OUT",
+                range: range,
+              },
+            ],
+          };
+        }
+
+        return {
+          suggestions: [
+            // TODO in progress
+            // {
+            //   label: "MATCH",
+            //   kind: monaco.languages.CompletionItemKind.Field,
+            //   documentation: "MATCH",
+            //   insertText: "MATCH {}",
+            //   range: range,
+            // },
+            // {
+            //   label: "CREATE",
+            //   kind: monaco.languages.CompletionItemKind.Field,
+            //   documentation: "CREATE",
+            //   insertText: "CREATE {}",
+            //   range: range,
+            // },
+          ],
+        };
+      },
+    });
+  }
+
+  return (
+    <Editor
+      height="100vh"
+      theme="vs-dark"
+      defaultLanguage="graphql"
+      beforeMount={beforeMount}
+      onMount={onMount}
+      options={{ fontSize: "18", tabSize: 2 }}
+    />
+  );
 }
 
 export default DGQLEditor;
