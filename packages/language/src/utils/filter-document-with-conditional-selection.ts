ab84dbb2aaf23b93945f077775e55c5304744ee2
Daniel Starns <danielstarns@hotmail.com>
2021-04-16 08:58:50 +0100

    refactor: use visitor to filter doc

--- packages/language/src/utils/filter-document-with-conditional-selection.ts
+++ packages/language/src/utils/filter-document-with-conditional-selection.ts
@@ -1,4 +1,9 @@
-import { DirectiveNode, DocumentNode, valueFromASTUntyped } from "graphql";
+import {
+  DirectiveNode,
+  DocumentNode,
+  valueFromASTUntyped,
+  visit,
+} from "graphql";
 
 function filterDocumentWithConditionalSelection({
   document,
@@ -7,82 +12,72 @@ function filterDocumentWithConditionalSelection({
   document: DocumentNode;
   variables: any;
 }): DocumentNode {
-  function replacer(key: any, value: any): any {
-    if (value === undefined) {
-      return undefined;
-    }
-
-    if (["string", "boolean", "number"].includes(typeof value)) {
-      return value;
-    }
-
-    const directives = value?.directives as undefined | DirectiveNode[];
-
-    if (!directives || !Array.isArray(directives)) {
-      return value;
-    }
-
-    let hasSeen = false;
-    let includeValue = true;
-
-    ["skip", "include"].forEach((type) => {
-      const found = directives.find((x) => x.name.value === type);
-
-      if (!found) {
+  const edited = visit(document, {
+    enter(node) {
+      if (node.kind !== "Field") {
         return;
       }
 
-      if (hasSeen) {
-        throw new Error("cannot @skip and @include at the same time");
+      const directives = node?.directives as undefined | DirectiveNode[];
+
+      if (!directives || !Array.isArray(directives)) {
+        return;
       }
 
-      hasSeen = true;
+      let hasSeen = false;
+      let includeValue = true;
 
-      const ifArg = found.arguments?.find((x) => x.name.value === "if");
+      ["skip", "include"].forEach((type) => {
+        const found = directives.find((x) => x.name.value === type);
 
-      if (!ifArg) {
-        throw new Error(`directive argument: @${type}(if: ) required`);
-      }
-
-      const ifValue = valueFromASTUntyped(ifArg.value, variables);
-
-      if (type === "skip") {
-        if (ifValue) {
-          includeValue = false;
+        if (!found) {
+          return;
         }
-      }
 
-      if (type === "include") {
-        if (!ifValue) {
-          includeValue = false;
+        if (hasSeen) {
+          throw new Error("cannot @skip and @include at the same time");
         }
+
+        hasSeen = true;
+
+        const ifArg = found.arguments?.find((x) => x.name.value === "if");
+
+        if (!ifArg) {
+          throw new Error(`directive argument: @${type}(if: ) required`);
+        }
+
+        const ifValue = valueFromASTUntyped(ifArg.value, variables);
+
+        if (type === "skip") {
+          if (ifValue) {
+            includeValue = false;
+          }
+        }
+
+        if (type === "include") {
+          if (!ifValue) {
+            includeValue = false;
+          }
+        }
+      });
+
+      if (includeValue) {
+        return {
+          ...node,
+          directives: [
+            ...((node?.directives || []) as DirectiveNode[]).filter(
+              (x) => !["skip", "include"].includes(x.name.value)
+            ),
+          ],
+        };
+      } else {
+        // delete this node
+        return null;
       }
-    });
+    },
+  });
 
-    if (includeValue) {
-      return {
-        ...value,
-        directives: [
-          ...((value?.directives || []) as DirectiveNode[]).filter(
-            (x) => !["skip", "include"].includes(x.name.value)
-          ),
-        ],
-      };
-    } else {
-      return undefined;
-    }
-  }
-
-  // TODO remove extra iteration - something wrong with this
-  return JSON.parse(
-    JSON.stringify(JSON.parse(JSON.stringify(document, replacer)), (key, v) => {
-      if (key === "selections") {
-        return v.filter(Boolean);
-      }
-
-      return v;
-    })
-  ) as DocumentNode;
+  return edited;
 }
 
 export default filterDocumentWithConditionalSelection;
