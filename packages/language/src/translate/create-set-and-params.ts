19f0472b97da19c1469f75e633af98e2a0f4bc4f
Daniel Starns <danielstarns@hotmail.com>
2021-04-18 13:04:28 +0100

    feat: add validate directive

--- packages/language/src/translate/create-set-and-params.ts
+++ packages/language/src/translate/create-set-and-params.ts
@@ -1,4 +1,9 @@
-import { ArgumentNode, FieldNode, valueFromASTUntyped } from "graphql";
+import {
+  ArgumentNode,
+  DirectiveNode,
+  FieldNode,
+  valueFromASTUntyped,
+} from "graphql";
 
 function createSetAndParams({
   setSelections,
@@ -13,19 +18,20 @@ function createSetAndParams({
   let params = {};
 
   setSelections?.forEach((selection) => {
-    const uuidDirective = selection.directives?.find(
-      (x) => x.name.value === "uuid"
-    );
+    const directives = (selection.directives || []) as DirectiveNode[];
+
+    const uuidDirective = directives.find((x) => x.name.value === "uuid");
     if (uuidDirective) {
       strs.push(`SET ${varName}.${selection.name.value} = randomUUID()`);
       return;
     }
 
-    const dateTimeDirective = selection.directives?.find(
+    const dateTimeDirective = directives.find(
       (x) => x.name.value === "datetime"
     );
-    const dateDirective = selection.directives?.find(
-      (x) => x.name.value === "date"
+    const dateDirective = directives.find((x) => x.name.value === "date");
+    const validateDirectives = directives.filter(
+      (x) => x.name.value === "validate"
     );
 
     const valueArg = (selection?.arguments || [])?.find(
@@ -40,9 +46,6 @@ function createSetAndParams({
     }
 
     const paramName = `${varName}_set_${selection.name.value}`;
-    if (valueArg) {
-      params[paramName] = valueFromASTUntyped(valueArg.value, variables);
-    }
 
     if (dateTimeDirective) {
       strs.push(
@@ -63,6 +66,134 @@ function createSetAndParams({
 
       return;
     }
+
+    const value = valueFromASTUntyped(valueArg.value, variables);
+    params[paramName] = value;
+
+    validateDirectives.forEach((direct) => {
+      const args = (direct.arguments || []) as ArgumentNode[];
+      const typeArg = args.find((x) => x.name.value === "type");
+      const errorArg = args.find((x) => x.name.value === "error");
+      const requiredArg = args.find((x) => x.name.value === "required");
+      const typeArgValue = typeArg
+        ? valueFromASTUntyped(typeArg.value, variables)
+        : undefined;
+      const errorArgValue = errorArg
+        ? valueFromASTUntyped(errorArg.value, variables)
+        : undefined;
+      const requiredArgValue = requiredArg
+        ? valueFromASTUntyped(requiredArg.value, variables)
+        : undefined;
+
+      try {
+        if (typeArgValue) {
+          const expectedTypes = ["String", "Number", "Boolean"];
+          if (!expectedTypes.includes(typeArgValue)) {
+            throw new Error(`@validate invalid type '${typeArgValue}'`);
+          }
+
+          const type: "String" | "Number" | "Boolean" = typeArgValue;
+
+          if (type === "String") {
+            if (typeof value !== "string") {
+              throw new Error(
+                `${selection.name.value} @validate invalid type expected String`
+              );
+            }
+
+            const minLenArg = args.find((x) => x.name.value === "minLength");
+            const maxLenArg = args.find((x) => x.name.value === "maxLength");
+            const regexArg = args.find((x) => x.name.value === "regex");
+            const minLenValue = minLenArg
+              ? valueFromASTUntyped(minLenArg.value, variables)
+              : undefined;
+            const maxLenValue = maxLenArg
+              ? valueFromASTUntyped(maxLenArg.value, variables)
+              : undefined;
+            const regexValue = regexArg
+              ? valueFromASTUntyped(regexArg.value, variables)
+              : undefined;
+
+            if (typeof minLenValue === "number") {
+              if (value.length < minLenValue) {
+                throw new Error(
+                  `${selection.name.value} @validate invalid value, expected minLength ${minLenValue}`
+                );
+              }
+            }
+
+            if (typeof maxLenValue === "number") {
+              if (value.length > maxLenValue) {
+                throw new Error(
+                  `${selection.name.value} @validate invalid value, expected maxLength ${maxLenValue}`
+                );
+              }
+            }
+
+            if (typeof regexValue === "string") {
+              const regex = new RegExp(regexValue);
+
+              if (!regex.test(value)) {
+                throw new Error(
+                  `${selection.name.value} @validate invalid value, incorrect match`
+                );
+              }
+            }
+          }
+
+          if (type === "Number") {
+            if (typeof value !== "number") {
+              throw new Error(
+                `${selection.name.value} @validate invalid type expected Number`
+              );
+            }
+
+            const minArg = args.find((x) => x.name.value === "min");
+            const maxArg = args.find((x) => x.name.value === "max");
+            const minValue = minArg
+              ? valueFromASTUntyped(minArg.value, variables)
+              : undefined;
+            const maxValue = maxArg
+              ? valueFromASTUntyped(maxArg.value, variables)
+              : undefined;
+
+            if (typeof minValue === "number") {
+              if (value < minValue) {
+                throw new Error(
+                  `${selection.name.value} @validate invalid value, expected min ${minValue}`
+                );
+              }
+            }
+
+            if (typeof maxValue === "number") {
+              if (value > maxValue) {
+                throw new Error(
+                  `${selection.name.value} @validate invalid value, expected max ${maxValue}`
+                );
+              }
+            }
+          }
+
+          if (type === "Boolean") {
+            if (typeof value !== "boolean") {
+              throw new Error(
+                `${selection.name.value} @validate invalid type expected Boolean`
+              );
+            }
+          }
+        }
+
+        if (requiredArgValue && value === undefined) {
+          throw new Error(`${selection.name.value} @validate required`);
+        }
+      } catch (error) {
+        if (errorArgValue) {
+          throw new Error(errorArgValue);
+        } else {
+          throw error;
+        }
+      }
+    });
 
     strs.push(`SET ${varName}.${selection.name.value} = $params.${paramName}`);
   });
--- packages/language/tests/unit/translate/create-set-and-params.test.ts
+++ packages/language/tests/unit/translate/create-set-and-params.test.ts
@@ -1,7 +1,453 @@
 import { createSetAndParams } from "../../../src/translate";
+import gql from "graphql-tag";
+import { FieldNode } from "graphql";
 
 describe("createSetAndParams", () => {
   test("should be a function", () => {
     expect(createSetAndParams).toBeInstanceOf(Function);
+  });
+
+  describe("@validate", () => {
+    test("should throw invalid type", () => {
+      const selection = gql`
+        {
+          SET {
+            test(value: $test) @validate(type: INVALID)
+          }
+        }
+      `;
+
+      const setSelections = ((((selection
+        ?.definitions[0] as unknown) as FieldNode).selectionSet
+        ?.selections as FieldNode[])[0] as FieldNode).selectionSet
+        ?.selections as FieldNode[];
+
+      expect(() =>
+        createSetAndParams({
+          setSelections,
+          varName: "test",
+          variables: {
+            test: "123",
+          },
+        })
+      ).toThrow("@validate invalid type 'INVALID'");
+    });
+
+    describe("String", () => {
+      test("should throw when value is not a string", () => {
+        const selection = gql`
+          {
+            SET {
+              test(value: $test) @validate(type: String)
+            }
+          }
+        `;
+
+        const setSelections = ((((selection
+          ?.definitions[0] as unknown) as FieldNode).selectionSet
+          ?.selections as FieldNode[])[0] as FieldNode).selectionSet
+          ?.selections as FieldNode[];
+
+        expect(() =>
+          createSetAndParams({
+            setSelections,
+            varName: "test",
+            variables: {
+              test: 123,
+            },
+          })
+        ).toThrow("test @validate invalid type expected String");
+      });
+
+      test("should throw when value is not a string (with custom error)", () => {
+        const msg = "SET test not a string";
+
+        const selection = gql`
+          {
+            SET {
+              test(value: $test) @validate(type: String, error: "${msg}")
+            }
+          }
+        `;
+
+        const setSelections = ((((selection
+          ?.definitions[0] as unknown) as FieldNode).selectionSet
+          ?.selections as FieldNode[])[0] as FieldNode).selectionSet
+          ?.selections as FieldNode[];
+
+        expect(() =>
+          createSetAndParams({
+            setSelections,
+            varName: "test",
+            variables: {
+              test: 123,
+            },
+          })
+        ).toThrow(msg);
+      });
+
+      test("should throw when value is less than minLength", () => {
+        const minLength = 2;
+
+        const selection = gql`
+          {
+            SET {
+              test(value: $test) 
+                @validate(type: String) 
+                @validate(type: String, minLength: ${minLength})
+
+            }
+          }
+        `;
+
+        const setSelections = ((((selection
+          ?.definitions[0] as unknown) as FieldNode).selectionSet
+          ?.selections as FieldNode[])[0] as FieldNode).selectionSet
+          ?.selections as FieldNode[];
+
+        expect(() =>
+          createSetAndParams({
+            setSelections,
+            varName: "test",
+            variables: {
+              test: "a",
+            },
+          })
+        ).toThrow(
+          `test @validate invalid value, expected minLength ${minLength}`
+        );
+      });
+
+      test("should throw when value is grater than maxLength", () => {
+        const maxLength = 2;
+
+        const selection = gql`
+          {
+            SET {
+              test(value: $test)
+                @validate(type: String)
+                @validate(type: String, maxLength: ${maxLength})
+            }
+          }
+        `;
+
+        const setSelections = ((((selection
+          ?.definitions[0] as unknown) as FieldNode).selectionSet
+          ?.selections as FieldNode[])[0] as FieldNode).selectionSet
+          ?.selections as FieldNode[];
+
+        expect(() =>
+          createSetAndParams({
+            setSelections,
+            varName: "test",
+            variables: {
+              test: "abc",
+            },
+          })
+        ).toThrow(
+          `test @validate invalid value, expected maxLength ${maxLength}`
+        );
+      });
+
+      test("should throw when value does not match regex", () => {
+        const regex = "user-.*";
+
+        const selection = gql`
+          {
+            SET {
+              test(value: $test) @validate(type: String, regex: "${regex}")
+            }
+          }
+        `;
+
+        const setSelections = ((((selection
+          ?.definitions[0] as unknown) as FieldNode).selectionSet
+          ?.selections as FieldNode[])[0] as FieldNode).selectionSet
+          ?.selections as FieldNode[];
+
+        expect(() =>
+          createSetAndParams({
+            setSelections,
+            varName: "test",
+            variables: {
+              test: "not-valid-regex",
+            },
+          })
+        ).toThrow(`test @validate invalid value, incorrect match`);
+      });
+
+      test("should throw when directive is stacked", () => {
+        const regex = "user-.*";
+        const msg = "Name must start with user-";
+
+        const selection = gql`
+          {
+            SET {
+              test(value: $test)
+                @validate(type: String)
+                @validate(type: String, minLength: 2)
+                @validate(type: String, maxLength: 100)
+                @validate(type: String, regex: "${regex}", error: "${msg}")
+            }
+          }
+        `;
+
+        const setSelections = ((((selection
+          ?.definitions[0] as unknown) as FieldNode).selectionSet
+          ?.selections as FieldNode[])[0] as FieldNode).selectionSet
+          ?.selections as FieldNode[];
+
+        expect(() =>
+          createSetAndParams({
+            setSelections,
+            varName: "test",
+            variables: {
+              test: "not-valid-regex",
+            },
+          })
+        ).toThrow(msg);
+      });
+    });
+
+    describe("Number", () => {
+      test("should throw when value is not a number", () => {
+        const selection = gql`
+          {
+            SET {
+              test(value: $test) @validate(type: Number)
+            }
+          }
+        `;
+
+        const setSelections = ((((selection
+          ?.definitions[0] as unknown) as FieldNode).selectionSet
+          ?.selections as FieldNode[])[0] as FieldNode).selectionSet
+          ?.selections as FieldNode[];
+
+        expect(() =>
+          createSetAndParams({
+            setSelections,
+            varName: "test",
+            variables: {
+              test: "123",
+            },
+          })
+        ).toThrow("test @validate invalid type expected Number");
+      });
+
+      test("should throw when value is not a number (with custom error)", () => {
+        const msg = "bad number";
+
+        const selection = gql`
+          {
+            SET {
+              test(value: $test) @validate(type: Number, error: "${msg}")
+            }
+          }
+        `;
+
+        const setSelections = ((((selection
+          ?.definitions[0] as unknown) as FieldNode).selectionSet
+          ?.selections as FieldNode[])[0] as FieldNode).selectionSet
+          ?.selections as FieldNode[];
+
+        expect(() =>
+          createSetAndParams({
+            setSelections,
+            varName: "test",
+            variables: {
+              test: "123",
+            },
+          })
+        ).toThrow(msg);
+      });
+
+      test("should throw when value is less then min", () => {
+        const min = 10;
+
+        const selection = gql`
+          {
+            SET {
+              test(value: $test) @validate(type: Number, min: ${min})
+            }
+          }
+        `;
+
+        const setSelections = ((((selection
+          ?.definitions[0] as unknown) as FieldNode).selectionSet
+          ?.selections as FieldNode[])[0] as FieldNode).selectionSet
+          ?.selections as FieldNode[];
+
+        expect(() =>
+          createSetAndParams({
+            setSelections,
+            varName: "test",
+            variables: {
+              test: 9,
+            },
+          })
+        ).toThrow(`test @validate invalid value, expected min ${min}`);
+      });
+
+      test("should throw when value is grater than max", () => {
+        const max = 10;
+
+        const selection = gql`
+          {
+            SET {
+              test(value: $test) @validate(type: Number, max: ${max})
+            }
+          }
+        `;
+
+        const setSelections = ((((selection
+          ?.definitions[0] as unknown) as FieldNode).selectionSet
+          ?.selections as FieldNode[])[0] as FieldNode).selectionSet
+          ?.selections as FieldNode[];
+
+        expect(() =>
+          createSetAndParams({
+            setSelections,
+            varName: "test",
+            variables: {
+              test: 11,
+            },
+          })
+        ).toThrow(`test @validate invalid value, expected max ${max}`);
+      });
+
+      test("should throw when directive is stacked", () => {
+        const min = 10;
+        const max = 20;
+        const msg = "error in this department";
+
+        const selection = gql`
+          {
+            SET {
+              test(value: $test) @validate(type: Number, min: ${min}) @validate(type: Number, max: ${max}, error: "${msg}")
+            }
+          }
+        `;
+
+        const setSelections = ((((selection
+          ?.definitions[0] as unknown) as FieldNode).selectionSet
+          ?.selections as FieldNode[])[0] as FieldNode).selectionSet
+          ?.selections as FieldNode[];
+
+        expect(() =>
+          createSetAndParams({
+            setSelections,
+            varName: "test",
+            variables: {
+              test: 30,
+            },
+          })
+        ).toThrow(msg);
+      });
+    });
+
+    describe("Boolean", () => {
+      test("should throw when value is not a boolean", () => {
+        const selection = gql`
+          {
+            SET {
+              test(value: $test) @validate(type: Boolean)
+            }
+          }
+        `;
+
+        const setSelections = ((((selection
+          ?.definitions[0] as unknown) as FieldNode).selectionSet
+          ?.selections as FieldNode[])[0] as FieldNode).selectionSet
+          ?.selections as FieldNode[];
+
+        expect(() =>
+          createSetAndParams({
+            setSelections,
+            varName: "test",
+            variables: {
+              test: 30,
+            },
+          })
+        ).toThrow("test @validate invalid type expected Boolean");
+      });
+
+      test("should throw when value is not a boolean (with custom error)", () => {
+        const msg = "spillage down isle 5";
+
+        const selection = gql`
+          {
+            SET {
+              test(value: $test) @validate(type: Boolean, error: "${msg}")
+            }
+          }
+        `;
+
+        const setSelections = ((((selection
+          ?.definitions[0] as unknown) as FieldNode).selectionSet
+          ?.selections as FieldNode[])[0] as FieldNode).selectionSet
+          ?.selections as FieldNode[];
+
+        expect(() =>
+          createSetAndParams({
+            setSelections,
+            varName: "test",
+            variables: {
+              test: 30,
+            },
+          })
+        ).toThrow(msg);
+      });
+    });
+
+    describe("No Type", () => {
+      test("should throw when value don't exist (required)", () => {
+        const selection = gql`
+          {
+            SET {
+              test(value: $test) @validate(required: true)
+            }
+          }
+        `;
+
+        const setSelections = ((((selection
+          ?.definitions[0] as unknown) as FieldNode).selectionSet
+          ?.selections as FieldNode[])[0] as FieldNode).selectionSet
+          ?.selections as FieldNode[];
+
+        expect(() =>
+          createSetAndParams({
+            setSelections,
+            varName: "test",
+            variables: {},
+          })
+        ).toThrow("test @validate required");
+      });
+
+      test("should throw when value don't exist (required) (with custom error)", () => {
+        const msg = "bad validate ooooo nooo!";
+
+        const selection = gql`
+          {
+            SET {
+              test(value: $test) @validate(required: true, error: "${msg}")
+            }
+          }
+        `;
+
+        const setSelections = ((((selection
+          ?.definitions[0] as unknown) as FieldNode).selectionSet
+          ?.selections as FieldNode[])[0] as FieldNode).selectionSet
+          ?.selections as FieldNode[];
+
+        expect(() =>
+          createSetAndParams({
+            setSelections,
+            varName: "test",
+            variables: {},
+          })
+        ).toThrow(msg);
+      });
+    });
   });
 });
