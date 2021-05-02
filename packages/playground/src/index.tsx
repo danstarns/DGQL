5cf6eecf440da00a27e46b6402f705ceb5f34e59
Daniel Starns <danielstarns@hotmail.com>
2021-05-02 12:22:48 +0100

    feat: add white logo and add it to playground

--- packages/playground/src/index.tsx
+++ packages/playground/src/index.tsx
@@ -5,31 +5,41 @@ import "./index.css";
 import { Neo4jProvider } from "use-neo4j";
 import { Neo4jScheme } from "use-neo4j/dist/neo4j-config.interface";
 import * as HistoryContext from "./HistoryContext";
+// @ts-ignore
+import Logo from "../../../docs/assets/dgql.png";
 
 const defaults = {
-    NEO_SCHEME: "bolt" as Neo4jScheme,
-    NEO_URL: "localhost",
-    NEO_PORT: "7687",
-    NEO_USER: "admin",
-    NEO_PASSWORD: "password",
-    NEO_DATABASE: "neo4j",
+  NEO_SCHEME: "bolt" as Neo4jScheme,
+  NEO_URL: "localhost",
+  NEO_PORT: "7687",
+  NEO_USER: "admin",
+  NEO_PASSWORD: "password",
+  NEO_DATABASE: "neo4j",
 };
 
 function Wrapper() {
-    return (
-        <Neo4jProvider
-            scheme={defaults.NEO_SCHEME}
-            host={defaults.NEO_URL}
-            port={defaults.NEO_PORT}
-            username={defaults.NEO_USER}
-            password={defaults.NEO_PASSWORD}
-            database={defaults.NEO_DATABASE}
-        >
-            <HistoryContext.Provider>
-                <App />
-            </HistoryContext.Provider>
-        </Neo4jProvider>
-    );
+  return (
+    <Neo4jProvider
+      logo={
+        <div className="w-100 d-flex flex-column mb-3">
+          <img src={Logo} style={{ maxWidth: "60%" }} className="mx-auto"></img>
+          <strong className="mx-auto">
+            <a href="https://github.com/danstarns/dgql">Github</a>
+          </strong>
+        </div>
+      }
+      scheme={defaults.NEO_SCHEME}
+      host={defaults.NEO_URL}
+      port={defaults.NEO_PORT}
+      username={defaults.NEO_USER}
+      password={defaults.NEO_PASSWORD}
+      database={defaults.NEO_DATABASE}
+    >
+      <HistoryContext.Provider>
+        <App />
+      </HistoryContext.Provider>
+    </Neo4jProvider>
+  );
 }
 
 ReactDOM.render(<Wrapper />, document.querySelector("#root"));
